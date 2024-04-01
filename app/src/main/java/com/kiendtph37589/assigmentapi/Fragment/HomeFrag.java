8d759e7146aae8b8d8107f337e5965fb7f194b00
kiendtph37589 <dinhtrongkien2004@gmail.com>
2024-04-01 16:59:38 +0700

    commit

--- app/src/main/java/com/kiendtph37589/assigmentapi/Fragment/HomeFrag.java
+++ app/src/main/java/com/kiendtph37589/assigmentapi/Fragment/HomeFrag.java
@@ -1,21 +1,29 @@
 package com.kiendtph37589.assigmentapi.Fragment;
 
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.Toast;
 import android.widget.ViewFlipper;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager2.widget.ViewPager2;
 
+import com.kiendtph37589.assigmentapi.Login;
 import com.kiendtph37589.assigmentapi.R;
 
 import java.util.List;
@@ -24,6 +32,7 @@ public class HomeFrag extends Fragment {
 //    ViewPager2 viewPager2;
     RecyclerView recycler_home;
     ViewFlipper v_flipper;
+    ImageView img_logout;
     private Handler handler = new Handler();
     @Nullable
     @Override
@@ -31,6 +40,37 @@ public class HomeFrag extends Fragment {
         View view = inflater.inflate(R.layout.frag_home, container, false);
 //        viewPager2 = view.findViewById(R.id.view_paper2);
         recycler_home = view.findViewById(R.id.recyclerView);
+        img_logout = view.findViewById(R.id.img_logout_home);
+        img_logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+                builder.setTitle("Thông báo!!!");
+                builder.setMessage("Are you sure?");
+                builder.setIcon(android.R.drawable.ic_popup_reminder);
+                builder.setCancelable(false);
+                builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        SharedPreferences preferences = getActivity().getSharedPreferences("checkbox", Context.MODE_PRIVATE);
+                        SharedPreferences.Editor editor = preferences.edit();
+                        editor.putString("remember", "false");
+                        editor.apply();
+                        startActivity(new Intent(getContext(),Login.class));
+
+                    }
+                });
+                builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+
+                Dialog dialog = builder.create();
+                dialog.show();
+            }
+        });
 
         //RecyclerView
         LinearLayoutManager layoutManager = new LinearLayoutManager(getContext(),RecyclerView.VERTICAL,false);
